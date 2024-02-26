50f0d490a521cd54ddc096d3786b08dda513a2f3
nadia <anabelle0025@yahoo.fr>
2024-02-26 16:50:57 +0100

    deux

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -42,16 +42,5 @@ class UserController extends AbstractController
         ]);
 
 }
-// #[Route('/user/login', name: 'app_login')]
-//     public function login(AuthenticationUtils $authenticationUtils): Response
-//     {
-//     // get the login error if there is one
-//     $error = $authenticationUtils->getLastAuthenticationError();
-//     // last username entered by the user
-//     $lastUsername = $authenticationUtils->getLastUsername();
-//     return $this->render('security/login.html.twig', [
-//     'last_username' => $lastUsername,
-//     'error' => $error,
-//     ]);
-//     }
+
 }
